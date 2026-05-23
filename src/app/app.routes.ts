6aab8c426da8655f5805af806b436a8fec1f52dd
SamuelRodríguezC <samuelrc250@gmail.com>
2026-05-23 12:02:01 -0500

    chore(rutas): agregar esqueleto base para el enrutamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+//   {
+//     path: '',
+//     loadComponent: () =>
+//       import('./layout/shell/shell.component')
+//         .then(c => c.ShellComponent),
+
+//     children: [
+//       {
+//         path: '',
+//         loadChildren: () =>
+//           import('./features/home/routes/home.routes')
+//             .then(r => r.HOME_ROUTES),
+//       },
+
+//       {
+//         path: 'personajes',
+//         loadChildren: () =>
+//           import('./features/characters/routes/characters.routes')
+//             .then(r => r.CHARACTERS_ROUTES),
+//       },
+
+//       {
+//         path: 'planetas',
+//         loadChildren: () =>
+//           import('./features/planets/routes/planets.routes')
+//             .then(r => r.PLANETS_ROUTES),
+//       },
+//     ],
+//   },
+
+//   {
+//     path: '**',
+//     loadChildren: () =>
+//       import('./features/not-found/routes/not-found.routes')
+//         .then(r => r.NOT_FOUND_ROUTES),
+//   },
+];
